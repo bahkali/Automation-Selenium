e90cb9b760e796c60abe4c148c6310c5c075ca0a
kaly <kalimamadou@gmail.com>
2021-01-09 19:08:46 -0800

    Manupulating element in the page by inputting text in textbox and clicking the button

--- automation.py
+++ automation.py
@@ -3,10 +3,18 @@ from selenium import webdriver
 chrome_browser = webdriver.Chrome('./drivers/chromedriver')
 
 # Maximize the window
-chrome_browser.maximize_window()
+# chrome_browser.maximize_window()
 chrome_browser.get('https://www.seleniumeasy.com/test/basic-first-form-demo.html')
 # check the title content
-assert 'Selenium Easy Demo - Simple Form to Automate using Selenium' in chrome_browser.title
+# assert 'Selenium Easy Demo - Simple Form to Automate using Selenium' in chrome_browser.title
+add_close_button = chrome_browser.find_element_by_id('at-cv-lightbox-close')
+add_close_button.click()
 
 button = chrome_browser.find_element_by_class_name("btn-default")
-print(button.get_attribute('innerHTML'))
+# print(button.get_attribute('innerHTML'))
+
+user_message = chrome_browser.find_element_by_id('user-message')
+user_message.clear()
+user_message.send_keys('Jessica what are you eating')
+
+button.click()
