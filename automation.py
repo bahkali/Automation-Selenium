fa06a73941bf88012cd96fe984e3cf73e5138c0a
kaly <kalimamadou@gmail.com>
2021-01-09 18:51:43 -0800

    grab an element from the page in a browser using selenium

--- automation.py
+++ automation.py
@@ -2,5 +2,11 @@ from selenium import webdriver
 
 chrome_browser = webdriver.Chrome('./drivers/chromedriver')
 
+# Maximize the window
 chrome_browser.maximize_window()
 chrome_browser.get('https://www.seleniumeasy.com/test/basic-first-form-demo.html')
+# check the title content
+assert 'Selenium Easy Demo - Simple Form to Automate using Selenium' in chrome_browser.title
+
+button = chrome_browser.find_element_by_class_name("btn-default")
+print(button.get_attribute('innerHTML'))
